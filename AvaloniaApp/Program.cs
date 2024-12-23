5f85debda0a96e7fc2179804cf0879ada4fb10c8
ProstoKylinar <102613624+ProstoKylinar@users.noreply.github.com>
2024-12-23 16:59:53 +0300

    Add files via upload

---
+++ AvaloniaApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using System;
+
+namespace AvaloniaApp;
+
+class Program
+{
+    [STAThread]
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Application started");
+
+        BuildAvaloniaApp()
+            .StartWithClassicDesktopLifetime(args);
+    }
+
+    public static AppBuilder BuildAvaloniaApp()
+        => AppBuilder.Configure<App>()
+                     .UsePlatformDetect()
+                     .LogToTrace();
+}
+
